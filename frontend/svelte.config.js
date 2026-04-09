88bab22476d0bd82f5139bd2eea0c15a9967c014
Deepak-GitHub1474 <deep84chaudhary@gmail.com>
2026-04-09 09:07:59 +0530

    chore-frontend: Remove '@sveltejs/adapter-auto' and update configuration for adapter selection
    - Removed '@sveltejs/adapter-auto' from package.json and pnpm-lock.yaml.
    
    - Updated svelte.config.js to conditionally use '@sveltejs/adapter-node' or '@sveltejs/adapter-vercel' based on the environment.

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,15 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+const adapter = process.env.VERCEL ? adapterVercel() : adapterNode();
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter,
 		alias: {
 			$src: path.resolve(__dirname, 'src')
 		}
